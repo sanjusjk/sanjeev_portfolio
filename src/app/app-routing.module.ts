9a4462b5dc6aac08e62f2148d57f024a2e799011
Sanjeev Jyothikrishnan E <sanju.jyothikrishnan@gmail.com>
2023-07-26 19:12:27 +0530

    check in codes after adding skills component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'skills', component: SkillsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
